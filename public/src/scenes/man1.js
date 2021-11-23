c4a8b02e46a9df8bea8552cb92fa213a67ba9f27
huyenpt21 <huyenpthe140045@fpt.edu.vn>
2021-11-23 22:59:01 +0700

    tile image

--- public/src/scenes/man1.js
+++ public/src/scenes/man1.js
@@ -6,9 +6,28 @@ class man1 extends Phaser.Scene {
     }
 
     preload() {
+        // this.load.image("tiles", "assets/nature_tile.png");
+        this.load.image("tiles", "assets/tiles.png");
+        this.load.tilemapTiledJSON("SeaMapDemo23114", "assets/SeaMapDemo23114.json");
+
 
     }
-    create() {}
+    create() {
+
+        const map = this.add.tilemap("SeaMapDemo23114");
+        const natural_tile = map.addTilesetImage("tiles");
+        // const autumn_tile = map.addTilesetImage("nature-tileset-autumn");
+         
+        const ObjectLayer = map.createLayer("sea_bg", [natural_tile]);
+        const worldLayer = map.createLayer("island", [natural_tile]);
+        // const img = this.add.image(0,0,'tiles');
+
+        // const map = this.make.tilemap({ key: 'map'});
+        // const tileset = map.addTilesetImage('nature-tile', 'tiles');
+
+        // const belowLayer = map.createStaticLayer('sea_background', tileset, 0, 0);
+        // const islandLayer = map.createStaticLayer('Island', tileset, 0,0);
+    }
 
     update() {}
 }
