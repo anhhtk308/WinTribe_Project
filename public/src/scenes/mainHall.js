fea6d96e89032da8412fdc12704b2feeedab3d80
thaodp2 <phuongthaoybyb@gmail.com>
2021-12-02 22:47:44 +0700

    edit animation

--- public/src/scenes/mainHall.js
+++ public/src/scenes/mainHall.js
@@ -90,25 +90,25 @@ class mainHall extends Phaser.Scene {
         // this.player.;
         // this.player.setCollideWorldBounds(true);
         // this.player.setGravityY(1000);
-        // this.anims.create({
-        //     key: 'left',
-        //     frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
-        //
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [ { key: 'player', frame: 4 } ],
-        //     frameRate: 20
-        // });
-        //
-        // this.anims.create({
-        //     key: 'right',
-        //     frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        //     frameRate: 10,
-        //     repeat: -1
-        // });
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'player', frame: 4 } ],
+            frameRate: 20
+        });
+        
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
         // this.camera.follow(this.player, Phaser.Cameras.FOLLOW_LOCKON, 0.1, 0.1);
         this.cameras.main.startFollow(this.player);
 
@@ -120,27 +120,33 @@ class mainHall extends Phaser.Scene {
     }
 
     update() {
-        this.player.setVelocity(0);
-
+        //this.player.setVelocity(0);
+         //this.player.anims.play("left",true);
         if (this.cursors.left.isDown)
         {
 
             this.player.setVelocityX(-150);
+            this.player.anims.play("left",true);
 
         }
         else if (this.cursors.right.isDown)
         {
             this.player.setVelocityX(150);
-
+            this.player.anims.play("right",true);
         }
 
-        if (this.cursors.up.isDown)
+        else if (this.cursors.up.isDown)
         {
             this.player.setVelocityY(-150);
+            this.player.anims.play("turn",true);
         }
         else if (this.cursors.down.isDown)
         {
             this.player.setVelocityY(150);
+        } else {
+            this.player.setVelocityY(0);
+            this.player.setVelocityX(0);
+            this.player.anims.play("turn",true);
         }
 
     }
